4088c679073292183cbb3919a274f7cd2eb76841
pasyaa <mhmdpsyh@gmail.com>
2026-06-02 17:44:45 +0700

    deklarasi array data dengan panjang index 3

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -3,3 +3,10 @@
 #include <array>
 using namespace std;
 
+int main()
+{
+    cout << "Awal program" << endl; //penanda 1
+    try
+    {
+        //deklarasi array data dengan panjang index 3
+        
