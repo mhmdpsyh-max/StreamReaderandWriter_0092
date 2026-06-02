983b48389d695cce300fdcc8f1b4104baf673e5e
pasyaa <mhmdpsyh@gmail.com>
2026-06-02 18:01:40 +0700

    kondisi jika throw bertipe data selain integer

--- ManualExceotion.cpp
+++ ManualExceotion.cpp
@@ -11,4 +11,9 @@ int main()
         cout << "Pernyataan tidak akan ditampilkan" << endl;
     }
     //kondisi jika throw yang bertipe data integer
+    catch (int a)
+    {
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    //kondisi jika throw bertipe data selain integer
     
