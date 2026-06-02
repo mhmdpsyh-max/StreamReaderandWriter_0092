beb4b9f969c228f5ba44648830e33f7aabf98968
pasyaa <mhmdpsyh@gmail.com>
2026-06-02 18:12:34 +0700

    string ambilProduk(size_t nomorRak) {

--- tugasprak.cpp
+++ tugasprak.cpp
@@ -6,3 +6,15 @@
 #include <stdexcept>
 using namespace std;
 
+class TokoElektronik {
+    private:
+    array<string, 3> barang;
+    public:
+    TokoElektronik() {
+        barang[0] = "Laptop";
+        barang[1] = "Smartphone";
+        barang[2] = "Printer";
+    }
+
+    string ambilProduk(size_t nomorRak) {
+        
